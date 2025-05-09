cda4729ffd221cc79c7c8a00864253a4a7167a9b
Thiago Nunes <thiago.nunes@nubank.com.br>
2025-05-09 19:33:20 -0300

    improvements

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import { Typography, Select, Button } from 'antd';
 import './App.css';
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 function App() {
 
   const [note, setNote] = useState('C');
+  const [field, setField] = useState([]);
+  const [grades, setGrades] = useState([1, 5, 4]);
 
   const getHarmonicField = () => {
     const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
@@ -28,20 +30,26 @@ function App() {
       scale[index] += '°';
     });
 
-    console.log(scale);
-    return scale;
+    const scaleWithGrades = scale.map((chord, index) => {
+      if (grades.includes(index + 1)) {
+        return chord;
+      }
+      return '';
+    });
+
+    setField(scaleWithGrades);
   };
 
   return (
-    <div className="App">
-      <Title style={{ textAlign: 'center' }}>
+    <div className="App" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Title style={{ textAlign: 'center'}}>
         Thiaguera Music App
       </Title>
       <Select
         placeholder="Select note"
         value={note}
         onChange={setNote}
-        style={{ width: 200, marginRight: 10 }}
+        style={{ width: 200, marginBottom: 10 }}
         options={[
           { value: 'C', label: 'C' },
           { value: 'D', label: 'D' },
@@ -52,9 +60,30 @@ function App() {
           { value: 'B', label: 'B' },
         ]}
       />
-      <Button type="primary" onClick={getHarmonicField}>
+      <Select
+        mode="multiple"
+        placeholder="Select Grades"
+        onChange={setGrades}
+        defaultValue={[1, 5, 4]}
+        style={{ width: 200, marginBottom: 10 }}
+        options={[
+          { value: 1, label: '1' },
+          { value: 2, label: '2' },
+          { value: 3, label: '3' },
+          { value: 4, label: '4' },
+          { value: 5, label: '5' },
+          { value: 6, label: '6' },
+          { value: 7, label: '7' },
+        ]}
+      />
+      <Button type="primary" onClick={getHarmonicField} style={{ marginBottom: 20 }}>
         Get Harmonic Field
       </Button>
+      <div>
+        {field.map((chord, index) => (
+          <Text key={index} style={{ display: 'block', textAlign: 'center', fontSize: 40 }}>{chord}</Text>
+        ))}
+      </div>
     </div>
   );
 }
