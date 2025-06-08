6f23df8ae1c6671618728119982b08f10f06819d
Thiago Nunes <thiago.nunes@nubank.com.br>
2025-06-08 14:06:42 -0300

    fix bug

--- src/functions/harmonicField.js
+++ src/functions/harmonicField.js
@@ -20,15 +20,21 @@ const harmonicField = (noteValue, grades) => {
     let suffix = '';
     if ([2, 3, 6].includes(grade)) suffix += 'm';
     if ([7].includes(grade)) suffix += '°';
-    if (typeof grades[index] === 'string' && grades[index].includes('vii')) suffix += '7';
-    if (typeof grades[index] === 'string' && grades[index].includes('(-m)')) suffix = suffix.replace('m', '');
-    if (typeof grades[index] === 'string' && grades[index].includes('(m)')) suffix += 'm';
     scale[grade - 1] += suffix;
   });
 
   const scaleWithGrades = numericGrade.map(grade => scale[grade - 1] || '');
 
-  return scaleWithGrades;
+  const formatedScaleWithGrades = grades.map((grade, index) => {
+    let formattedGrade = scaleWithGrades[index];
+    const gradeStr = String(grade);
+    if (gradeStr.includes('vii')) formattedGrade += '7';
+    if (gradeStr.includes('(-m)')) formattedGrade = formattedGrade.replace('m', '');
+    if (gradeStr.includes('(m)')) formattedGrade += 'm';
+    return formattedGrade;
+  });
+
+  return formatedScaleWithGrades;
 };
 
 export default harmonicField;
